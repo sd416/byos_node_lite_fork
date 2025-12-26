002fd259a34ffc7e7dfc2d1097aca8bdbcd97ef8
Andrii Reinvald <andreinwald@gmail.com>
2025-12-26 11:01:19 +0100

    Cleanup page before render and fixing tests

--- src/Screen/RenderHTML.ts
+++ src/Screen/RenderHTML.ts
@@ -1,6 +1,6 @@
-import puppeteer, {Page} from "puppeteer";
+import puppeteer, { Page } from "puppeteer";
 import fs from 'fs/promises';
-import {ASSETS_FOLDER, IS_TEST_ENV} from "Config.js";
+import { ASSETS_FOLDER, IS_TEST_ENV } from "Config.js";
 
 export const BASE_URL_CHROME = 'http://localhost';
 
@@ -8,24 +8,28 @@ export const BASE_URL_CHROME = 'http://localhost';
 let page: Page;
 
 export async function initPuppeteer() {
+    if (page) {
+        console.warn('Puppeteer already initialized');
+        return;
+    }
     if (!IS_TEST_ENV) {
         console.log('start of Puppeteer init');
     }
     const browser = await puppeteer.launch({
-            headless: true,
-            protocolTimeout: 5000,
-            timeout: 5000,
-            args: [
-                '--no-sandbox',
-                '--disable-web-security',
-                '--disable-gpu',
-            ]
-        }
+        headless: true,
+        protocolTimeout: 5000,
+        timeout: 5000,
+        args: [
+            '--no-sandbox',
+            '--disable-web-security',
+            '--disable-gpu',
+        ]
+    }
     );
     page = await browser.newPage();
-    await page.setViewport({width: 800, height: 480});
+    await page.setViewport({ width: 800, height: 480 });
     await page.setRequestInterception(true);
-    page.on('pageerror', ({message}) => console.error('error:', message));
+    page.on('pageerror', ({ message }) => console.error('error:', message));
     page.on('requestfailed', request => console.log(`Failed: ${request.failure()?.errorText} ${request.url()}`));
     // page.on('console', message => console.log('console: ', message.text()));
     page.on('request', async (interceptedRequest) => {
@@ -40,7 +44,7 @@ export async function initPuppeteer() {
         try {
             const filePathPart = url.replace(BASE_URL_CHROME + '/assets/', '/')
             const file = await fs.readFile(ASSETS_FOLDER + filePathPart);
-            await interceptedRequest.respond({body: file});
+            await interceptedRequest.respond({ body: file });
         } catch (error) {
             await interceptedRequest.abort();
         }
@@ -52,7 +56,8 @@ export async function initPuppeteer() {
 
 
 export async function renderToImage(html: string) {
-    await page.setContent(html, {waitUntil: "load"});
+    await page.goto('about:blank');
+    await page.setContent(html, { waitUntil: "load" });
     const image: Uint8Array = await page.screenshot();
     return Buffer.from(image);
 }
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,9 @@
-import {defineConfig} from 'vitest/config'
+import { defineConfig } from 'vitest/config'
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
     plugins: [tsconfigPaths()],
     test: {
-        sequence: {
-            concurrent: true,
-        },
         env: {
             'SECRET_KEY': 'TEST_SECRET_KEY',
             'PUBLIC_URL_ORIGIN': 'TEST_PUBLIC_URL_ORIGIN',
